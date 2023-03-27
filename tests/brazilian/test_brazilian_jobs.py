ce0fdc7861c0d34fc136c8befebc22dd2a783375
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-03-27 15:44:34 -0300

    Testando req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+
+mock_translate = {
+    'title': 'Motorista',
+    'salary': '3000',
+    'type': 'full time',
+}
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(path)[1] == mock_translate
