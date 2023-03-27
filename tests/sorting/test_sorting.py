79a1fa8f43e26dbada539d2fa0ff747984bf16bb
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-03-27 17:31:56 -0300

    Tentando req 12 mais uma vez

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,14 +1,31 @@
 from src.pre_built.sorting import sort_by
 
 
-mock_order = {
-        'title': 'Web developer',
-        'min_salary': 'invalid',
-        'max_salary': 'invalid',
-    }
+mock_job = [
+    {"max_salary": 1000, "min_salary": 1, "date_posted": "2021-01-01"},
+    {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+    {"max_salary": 100, "min_salary": 10, "date_posted": "2022-01-01"},
+]
 
-path = 'tests/mocks/jobs.csv'
+job_min_salary = [
+    {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+    {"max_salary": 1000, "min_salary": 1, "date_posted": "2021-01-01"},
+    {"max_salary": 100, "min_salary": 10, "date_posted": "2022-01-01"},
+]
+
+job_by_date = [
+    {"max_salary": 200, "min_salary": 0, "date_posted": "2023-01-01"},
+    {"max_salary": 100, "min_salary": 10, "date_posted": "2022-01-01"},
+    {"max_salary": 1000, "min_salary": 1, "date_posted": "2021-01-01"},
+]
 
 
 def test_sort_by_criteria():
-    assert sort_by(path)[0] == mock_order
+    sort_by(mock_job, 'max_salary')
+    assert mock_job == mock_job
+
+    sort_by(mock_job, 'min_salary')
+    assert mock_job == job_min_salary
+
+    sort_by(mock_job, 'date_posted')
+    assert mock_job == job_by_date
